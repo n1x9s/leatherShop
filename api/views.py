481b6c9011a0fdac7a4696ca8ed06b226e18af22
niki <113206190+n1x9s@users.noreply.github.com>
2024-08-07 02:16:38 +0300

    updated redirect

--- api/views.py
+++ api/views.py
@@ -1,8 +1,6 @@
-from django.shortcuts import render
-from shop.models import Bag
 from rest_framework import viewsets
-from rest_framework.permissions import IsAdminUser
 
+from shop.models import Bag
 from .permissions import IsUserAdminOrReadOnly
 from .serializers import BagSerializer
 
--- leatherShop/settings.py
+++ leatherShop/settings.py
@@ -142,3 +142,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_REDIRECT_URL = 'shop:index'
 LOGOUT_REDIRECT_URL = 'shop:index'
+
+LOGIN_URL = 'personalAccount:signin'
